d275939127a338c7fdd7dd37fba1a9a77a4d4f07
vampire_pp_ <vampirepp11@gmail.com>
2021-12-14 12:07:06 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FootersComponent } from './components/footers/footers.component';
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
+import { Page3Component } from './components/page3/page3.component';
+import { Page4Component } from './components/page4/page4.component';
+import { Page5Component } from './components/page5/page5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FootersComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component,
+    Page5Component
   ],
   imports: [
     BrowserModule,
